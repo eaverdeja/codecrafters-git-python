38b2e58db13d2ec5a9d4009909673d35b812ef71
eaverdeja <eaverdeja@gmail.com>
2025-01-25 18:30:57 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,6 +3,14 @@ import os
 import zlib
 from hashlib import sha1
 from argparse import ArgumentParser
+from dataclasses import dataclass
+
+
+@dataclass
+class TreeEntry:
+    mode: str
+    name: str
+    sha_hash: str
 
 
 def main():
@@ -76,6 +84,52 @@ def main():
                     file.write(compressed_object)
             
             print(object_hash)
+        case "ls-tree":
+            parser = ArgumentParser(description="Prints the structure of a tree object.")
+            parser.add_argument('tree_hash')
+            parser.add_argument(
+                "--name-only",
+                action="store_true",
+                help="Specifies that only file/directory names should be outputted",
+            )
+            args = parser.parse_args(sys.argv[2:]) 
+            tree_hash = args.tree_hash
+           
+            # Fetch the trees contents from the filesystem
+            folder = tree_hash[0:2]
+            filename = tree_hash[2:] 
+            with open(f".git/objects/{folder}/{filename}", "rb") as file:
+                compressed_contents = file.read()
+            contents = zlib.decompress(compressed_contents)
+            
+            # Parse the content type and make sure it's a tree
+            end_of_type_marker = contents.find(b" ")
+            end_of_size_marker = contents.find(b"\x00")
+            content_type = contents[: end_of_type_marker].decode()
+            assert content_type == "tree"
+            
+            # Parse the entries
+            content_size = int(contents[end_of_type_marker + 1 : end_of_size_marker].decode())
+            entries = contents[end_of_size_marker + 1 : end_of_size_marker + content_size + 1]
+            parsed_entries = []
+            pos = 0
+            while entries := entries[pos:]:
+                end_of_mode_marker = entries.find(b" ")
+                end_of_name_marker = entries.find(b"\x00")
+                
+                mode = entries[: end_of_mode_marker]
+                name = entries[end_of_mode_marker + 1 : end_of_name_marker].decode()
+                # The SHA hash is 20 bytes in length
+                sha_hash = entries[end_of_name_marker + 1 : end_of_name_marker + 20 + 1]
+                
+                # +2 for the control markers
+                pos = len(mode) + len(name) + len(sha_hash) + 2
+                parsed_entries.append(TreeEntry(mode=mode, name=name, sha_hash=sha_hash))
+            
+            # Print out entry names
+            if args.name_only:
+                for entry in sorted(parsed_entries, key=lambda entry: entry.name):
+                    print(entry.name)
         case _:
             raise RuntimeError(f"Unknown command #{command}")
 
