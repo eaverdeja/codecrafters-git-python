ce199e3b0ba28cc9c8101035878a134cbf2b4cde
eaverdeja <eaverdeja@gmail.com>
2025-01-26 14:09:38 -0300

    formatting

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ class TreeEntry:
 
 def main():
     # https://blog.meain.io/2023/what-is-in-dot-git/
-    
+
     command = sys.argv[1]
     match command:
         case "init":
@@ -27,7 +27,7 @@ def main():
             print("Initialized git directory")
         case "cat-file":
             parser = ArgumentParser(description="Reads a git object")
-            parser.add_argument('object_hash')
+            parser.add_argument("object_hash")
             parser.add_argument(
                 "--content",
                 "-p",
@@ -38,25 +38,31 @@ def main():
             object_hash = args.object_hash
             folder = object_hash[0:2]
             filename = object_hash[2:]
-            
-            with open(f".git/objects/{folder}/{filename}", 'rb') as file:
+
+            with open(f".git/objects/{folder}/{filename}", "rb") as file:
                 compressed_contents = file.read()
-                
+
             contents = zlib.decompress(compressed_contents)
-            
+
             # Parse the content
             end_of_type_marker = contents.find(b" ")
             end_of_size_marker = contents.find(b"\x00")
-            content_type = contents[: end_of_type_marker].decode()
+            content_type = contents[:end_of_type_marker].decode()
             assert content_type == "blob"
-            
-            content_size = int(contents[end_of_type_marker + 1 : end_of_size_marker].decode())
-            contents = contents[end_of_size_marker + 1 : end_of_size_marker + content_size + 1].decode()
-            
+
+            content_size = int(
+                contents[end_of_type_marker + 1 : end_of_size_marker].decode()
+            )
+            contents = contents[
+                end_of_size_marker + 1 : end_of_size_marker + content_size + 1
+            ].decode()
+
             sys.stdout.write(contents)
         case "hash-object":
-            parser = ArgumentParser(description="Computes the SHA hash of a git object. Optionally writes the object.")
-            parser.add_argument('file_name')
+            parser = ArgumentParser(
+                description="Computes the SHA hash of a git object. Optionally writes the object."
+            )
+            parser.add_argument("file_name")
             parser.add_argument(
                 "--write",
                 "-w",
@@ -66,66 +72,74 @@ def main():
             args = parser.parse_args(sys.argv[2:])
             file_name = args.file_name
 
-            with open(file_name, 'rb') as file:
+            with open(file_name, "rb") as file:
                 contents = file.read()
-            
+
             content_length = len(contents)
-            blob_object = b'blob ' + str(content_length).encode() + b'\0' + contents
+            blob_object = b"blob " + str(content_length).encode() + b"\0" + contents
             object_hash = sha1(blob_object).hexdigest()
-            
+
             if args.write:
                 folder = object_hash[0:2]
                 filename = object_hash[2:]
                 if not os.path.isdir(f".git/objects/{folder}"):
                     os.mkdir(f".git/objects/{folder}")
-                
+
                 compressed_object = zlib.compress(blob_object)
-                with open(f".git/objects/{folder}/{filename}", 'wb') as file:
+                with open(f".git/objects/{folder}/{filename}", "wb") as file:
                     file.write(compressed_object)
-            
+
             print(object_hash)
         case "ls-tree":
-            parser = ArgumentParser(description="Prints the structure of a tree object.")
-            parser.add_argument('tree_hash')
+            parser = ArgumentParser(
+                description="Prints the structure of a tree object."
+            )
+            parser.add_argument("tree_hash")
             parser.add_argument(
                 "--name-only",
                 action="store_true",
                 help="Specifies that only file/directory names should be outputted",
             )
-            args = parser.parse_args(sys.argv[2:]) 
+            args = parser.parse_args(sys.argv[2:])
             tree_hash = args.tree_hash
-           
+
             # Fetch the trees contents from the filesystem
             folder = tree_hash[0:2]
-            filename = tree_hash[2:] 
+            filename = tree_hash[2:]
             with open(f".git/objects/{folder}/{filename}", "rb") as file:
                 compressed_contents = file.read()
             contents = zlib.decompress(compressed_contents)
-            
+
             # Parse the content type and make sure it's a tree
             end_of_type_marker = contents.find(b" ")
             end_of_size_marker = contents.find(b"\x00")
-            content_type = contents[: end_of_type_marker].decode()
+            content_type = contents[:end_of_type_marker].decode()
             assert content_type == "tree"
-            
+
             # Parse the entries
-            content_size = int(contents[end_of_type_marker + 1 : end_of_size_marker].decode())
-            entries = contents[end_of_size_marker + 1 : end_of_size_marker + content_size + 1]
+            content_size = int(
+                contents[end_of_type_marker + 1 : end_of_size_marker].decode()
+            )
+            entries = contents[
+                end_of_size_marker + 1 : end_of_size_marker + content_size + 1
+            ]
             parsed_entries = []
             pos = 0
             while entries := entries[pos:]:
                 end_of_mode_marker = entries.find(b" ")
                 end_of_name_marker = entries.find(b"\x00")
-                
-                mode = entries[: end_of_mode_marker]
+
+                mode = entries[:end_of_mode_marker]
                 name = entries[end_of_mode_marker + 1 : end_of_name_marker].decode()
                 # The SHA hash is 20 bytes in length
                 sha_hash = entries[end_of_name_marker + 1 : end_of_name_marker + 20 + 1]
-                
+
                 # +2 for the control markers
                 pos = len(mode) + len(name) + len(sha_hash) + 2
-                parsed_entries.append(TreeEntry(mode=mode, name=name, sha_hash=sha_hash))
-            
+                parsed_entries.append(
+                    TreeEntry(mode=mode, name=name, sha_hash=sha_hash)
+                )
+
             # Print out entry names
             if args.name_only:
                 for entry in sorted(parsed_entries, key=lambda entry: entry.name):
